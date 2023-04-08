8dd20cc3e73f7c20733be4bab5fba12e7c341b3f
Luisa Horowitz <118489479+luhoro@users.noreply.github.com>
2023-04-08 17:28:28 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ const Main = {
    //selecionar e armazenar os elementos html
    cacheSelectors: function () {
       this.$checkButton = document.querySelectorAll('.check')
+      this.$inputTask = document.querySelector('#inputTask')
+      this.$list = document.querySelector('#list')
+      this.$removeButtons = document.querySelectorAll('.remove')
    },
 
    //adicionar eventos de clique
@@ -19,22 +22,54 @@ const Main = {
       this.$checkButton.forEach(function(button) {
          button.onclick = self.Events.checkButton_click
       })
+
+      this.$inputTask.onkeypress = self.Events.inputTask_keypress.bind(this)
+
+      this.$removeButtons.forEach(function(button) {
+         button.onclick = self.Events.removeButton_click
+      })
    },
 
 
    Events: {
       checkButton_click: function(e){
          const li = e.target.parentElement
-         const isDone = li.classList.contains('done')
 
-         if (!isDone){
-            return li.classList.add('done')
-         }
-         li.classList.remove('done')
-      }
+         li.classList.toggle('done')
+      },
  
+      inputTask_keypress: function(e){ 
+         const key = e.key
+         const value = e.target.value
 
+         if (key === 'Enter') {
+            console.log(this)
+            this.$list.innerHTML += `
+            <li>
+               <div class="check"> </div>
+               <p class="task"> 
+               ${value} 
+               </p>
+               <button class="remove"> </button>
+            </li>
+            `
 
+            e.target.value = ''
+
+            this.cacheSelectors()
+            this.bindEvents()
+         }
+      },
+
+      removeButton_click: function (e) {
+         let li = e.target.parentElement
+
+         li.classList.add('removed')
+
+         setTimeout(function(){
+            li.classList.add('hidden')
+         }, 300)
+      }
 
    }
 
